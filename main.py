4124ee15755e1f6caccbd65d68c7a946e28f4eb4
JPablo67 <juanpa.995@gmail.com>
2024-09-10 20:51:11 -0500

    Linters

---
+++ main.py
@@ -0,0 +1,15 @@
+"""Esta clase suma dos numeros"""
+
+
+def suma(num1, num2):
+    """
+    Calculate the sum of two numbers
+
+    Args:
+        num1 (int or float): the first number.
+        num2 (int or float): the second number.
+
+    Returns:
+        int or float: The sum of num1 and num2.
+    """
+    return num1 + num2
