9a02da238470072ec0dc0b81f51954c48d0d0566
JPablo67 <juanpa.995@gmail.com>
2024-09-10 20:53:43 -0500

    Linters test

--- main.py
+++ main.py
@@ -2,5 +2,14 @@
 
 
 def suma(num1, num2):
-    
+    """
+    Calculate the sum of two numbers
+
+    Args:
+        num1 (int or float): the first number.
+        num2 (int or float): the second number.
+
+    Returns:
+        int or float: The sum of num1 and num2.
+    """
     return num1 + num2
